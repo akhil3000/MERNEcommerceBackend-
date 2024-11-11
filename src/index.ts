f9aa521bd3ce984725590cab31b745e2c2be0eb6
AkhilPolisetty <akhilpolisetty456@gmail.com>
2024-11-11 20:31:12 +0530

    backend configuration is done

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,16 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+dotenv.config();
+import {userRouter} from "./routes/user";
+
 
 const app=express();
+app.use(express.json());
+app.use(cors());
+app.use("/user",userRouter);
+mongoose.connect(process.env.MONGODBURI).then(()=>{
+    console.log("Connected to MongoDB");
+})
 
 app.listen(process.env.PORT,()=>console.log("Server Started"));
---
+++ src/routes/user.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const router=express.Router();
+
+router.post("/register",async()=>{
+
+    
+});
+
+export{router as userRouter};
